e23c226c118c31dfb5a6302f7b31c2bcafe21b21
Sahil <sahilbondre@gmail.com>
2020-02-26 23:02:04 +0530

    add basic scanner code

--- main.dart
+++ main.dart
@@ -5,14 +5,33 @@ main(List<String> args) {
     print("Usage: main [script]");
     exit(64);
   } else if (args.length == 1) {
-    File f = new File(args[0]);
-    String s = f.readAsStringSync();
-    print(s);
-  } else {
-    print("Welcome to interpreter v0.1.0");
-    while (true) {
-      stdout.write("> ");
-      print(stdin.readLineSync());
+    try {
+      File f = new File(args[0]);
+      run(f.readAsStringSync());
+    } on FileSystemException {
+      print("Error: File does not exist");
     }
+  } else {
+    runPrompt();
   }
+}
+
+void runPrompt() {
+  print("Welcome to interpreter v0.1.0");
+  while (true) {
+    stdout.write("> ");
+    print(stdin.readLineSync());
+  }
+}
+
+void run(String source) {
+  print(source);
+}
+
+void error(int line, String message) {
+  report(line, "", message);
+}
+
+void report(int line, String where, String message) {
+  print("Error on line $line: $where => $message");
 }
